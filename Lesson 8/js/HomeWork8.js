e68dc3c2bfe7025602fd1036e66410d5bfc26b05
Yuliya-github <59915033+Yuliya-github@users.noreply.github.com>
2020-04-22 11:08:19 +0300

    Update HomeWork8.js

--- Lesson 8/js/HomeWork8.js
+++ Lesson 8/js/HomeWork8.js
@@ -1,60 +1,23 @@
+    window.onload = () => {
+    const button = document.querySelector('#new-div'),
+    divContainer = document.createElement('div');
+    button.after(divContainer);
+    let counter = 0;
 
-
-        const createNewChild = () => {
-           const my_div = document.getElementById('my_div');
-
-           for ( newDiv=0; newDiv<10; newDiv++ ) { // пока получилось только добавлять сразу по 10 дивов одним кликом вместо того,чтобы добавлять максимум 10
-           const newDiv = document.createElement('div'); 
-           newDiv.innerHTML = "<h1>Привет!</h1>";
-           my_div.appendChild(newDiv);
+    button.onclick = () => {
+        if (counter<10) {
+            counter+=1;
+        const newDiv = document.createElement('div'); 
+        newDiv.innerHTML = "<h1>Привет!</h1>";
+        divContainer.appendChild(newDiv);
+        return counter;
+       
+     } else {
+         divContainer.remove();
         }
-        
-        } 
-             
-        const removeLastChild = () => {
-            const my_div = document.getElementById('my_div');
-            const newDiv = my_div.lastChild;
-            if (newDiv != null) {
-                    my_div.removeChild(newDiv);
-            }
+         
+    }
+}
 
-        }      
 
         
-
-
-/* Способы, которыми  я пробовала сделать ограничение в добавлении дивов
-
-let newDiv = 0;
-while (newDiv<10) {
-    newDiv = document.createElement('div');
-    newDiv.innerHTML = "<h1>Привет!</h1>";
-    my_div.appendChild(newDiv);
-    newDiv ++;
-    }
-    
-    
-    var newDiv = 0;
-    
-    do {  newDiv = document.createElement('div');
-    newDiv.innerHTML = "<h1>Привет!</h1>";
-    my_div.appendChild(newDiv);
-    newDiv ++;
-    } while (newDiv<3);
-    
-    
-    let newDiv = 0;
-    if (newDiv <= 3) {
-    newDiv = document.createElement('div');
-    newDiv.innerHTML = "<h1>Привет!</h1>";
-    my_div.appendChild(newDiv);
-    newDiv++;
-    }
-    
-      newDiv = 0;
-     do {  
-     newDiv ++;
-     }       
-     while (newDiv<10);
-                    
-*/
